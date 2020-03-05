190fe0b319ceb759f85ca56a0a244650016e3f72
gleb-zavizenov <g_zavizenov@fanshaweonline.ca>
2020-03-05 10:01:08 -0500

    Added npm and gulp

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const postcss = require("gulp-postcss");
+const nano = require("cssnano");
+const prefixer = require("autoprefixer");
+
+//define some tasks for GULP to run
+
+//line to complete and modify SASS files
+function compile(done) {
+    gulp.src('./sass/**/*.scss')
+        .pipe(sass())
+        .on('error', sass.logError)
+        .pipe(postcss([prefixer(),nano()]))
+        .pipe(gulp.dest('./css'))
+        done()
+}
+
+exports.compile = compile;
